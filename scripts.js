fa6116ae494ee6fec109df1d0a4d9d6a6baf315e
EmirlanDogdurbaev <jigit38@gmail.com>
2021-01-15 15:22:33 +0300

    declared variable

--- scripts.js
+++ scripts.js
@@ -1,5 +1,41 @@
-let first = "Emirlan";
+let first = "Shrek";
 let second = first;
 let third = second;
 let fourth = third;
 let fifth = fourth;
+console.log(fifth);
+
+let capital = "Bishkek";
+let country = "Kyrgyzstan";
+
+console.log(capital + " is the capital of " + country);
+
+let unit = "meters";
+let subject = "Distance";
+
+unit = "Selcia";
+subject = "temperature";
+
+unit = "kg";
+subject = "weight";
+
+console.log(subject + " is measuted in " + unit);
+
+let title = "Кip off the mask";
+let author = "Agata Kristi";
+
+title = "The art of influencing people";
+author = "Dale Breckenridge Carnegie";
+console.log("My favoutite book is " + title + "by " + author);
+
+console.log("My favourite song is \"Yesterday\" by Beatles")
+
+
+console.log("My favourite song is 'Yesterday' by Beatles")
+
+console.log('\I\'m the best')
+
+let artist ="Beatles"
+
+console.log('I\'m the fan of "' +  artist + '"!')
+
