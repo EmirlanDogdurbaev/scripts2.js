7a932d590f58de570243b8c8efad122475673a40
EmirlanDogdurbaev <jigit38@gmail.com>
2021-01-18 14:05:20 +0300

    did homework

--- scripts.js
+++ scripts.js
@@ -1,41 +1,96 @@
-let first = "Shrek";
-let second = first;
-let third = second;
-let fourth = third;
-let fifth = fourth;
-console.log(fifth);
+// let first = "Shrek";
+// let second = first;
+// let third = second;
+// let fourth = third;
+// let fifth = fourth;
+// console.log(fifth);
 
-let capital = "Bishkek";
-let country = "Kyrgyzstan";
+// let capital = "Bishkek";
+// let country = "Kyrgyzstan";
 
-console.log(capital + " is the capital of " + country);
+// console.log(capital + " is the capital of " + country);
 
-let unit = "meters";
-let subject = "Distance";
+// let unit = "meters";
+// let subject = "Distance";
 
-unit = "Selcia";
-subject = "temperature";
+// unit = "Selcia";
+// subject = "temperature";
 
-unit = "kg";
-subject = "weight";
+// unit = "kg";
+// subject = "weight";
 
-console.log(subject + " is measuted in " + unit);
+// console.log(subject + " is measuted in " + unit);
 
-let title = "Кip off the mask";
-let author = "Agata Kristi";
+// let title = "Кip off the mask";
+// let author = "Agata Kristi";
 
-title = "The art of influencing people";
-author = "Dale Breckenridge Carnegie";
-console.log("My favoutite book is " + title + "by " + author);
+// title = "The art of influencing people";
+// author = "Dale Breckenridge Carnegie";
+// console.log("My favoutite book is " + title + "by " + author);
 
-console.log("My favourite song is \"Yesterday\" by Beatles")
+// console.log("My favourite song is \"Yesterday\" by Beatles")
 
 
-console.log("My favourite song is 'Yesterday' by Beatles")
+// console.log("My favourite song is 'Yesterday' by Beatles")
 
-console.log('\I\'m the best')
+// console.log('\I\'m the best')
 
-let artist ="Beatles"
+// let artist ="Beatles"
 
-console.log('I\'m the fan of "' +  artist + '"!')
+// console.log('I\'m the fan of "' +  artist + '"!')
 
+
+// console.log(`I'm the fan of "${artist}" !`)
+
+// let movie = "Terminator"
+
+// console.log(`My favourite movie is "${movie}"`)
+// console.log(`My favourite movie is "` + movie + `"` )
+
+// let firstName = prompt("What is your name?")
+
+
+// console.log(`Your name is ${firstName}`)
+
+// let a = +prompt("Whate is A");
+// let b = +prompt("Whate is B");
+// let sum = a+b;
+// console.log(`The sum of two numbers is equal to ${sum}`)
+
+// let c = +prompt("Whate is c");
+// let d = +prompt("Whate is d");
+// let result = c * d;
+// console.log(`C multipled by D is equal to ${result} `)
+
+// let speed = prompt("Whate is Speed");
+// let time = prompt("Whate is time")
+
+// let distance = speed * time;
+// console.log(`Speed multipled by time is equal to ${distance} `)
+
+// var f = "f(x,y) = 2*x + 5/y";
+// var x = 500;
+// var y = 2.5;
+
+// f = f.replace(/f\(x,y\)\s=\s/g, ""); //удаляем лишнее
+// f = f.replace(/x/g, x); // заменяем подстроку x на значение переменной x
+// f = f.replace(/y/g, y); // тоже самое с y
+// f = eval(f); // вычисляем значение строкового выражения
+
+// console.log(f);
+
+
+// let S = +prompt("Whate is distance");
+// let T = +prompt("Whate is time");
+// let V = S / T;
+// console.log(`${V} is Speed`);
+
+// let V = +prompt("Whate is speed");
+// let T = +prompt("Whate is time");
+// let S = V * T;
+// console.log(`${S} is distance`);
+
+// let S = +prompt("Whate is distance");
+// let V = +prompt("Whate is speed");
+// let T = S / V;
+// console.log(`${T} is Speed`);
